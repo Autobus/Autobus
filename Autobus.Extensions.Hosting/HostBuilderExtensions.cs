6461002d2e4ca314a217ad35fca7272314fd4e02
Chandler Stowell <chandler14362@gmail.com>
2021-03-03 07:04:37 -0500

    fixed hosting extensions creating the autobus before its needed

--- Autobus.Extensions.Hosting/HostBuilderExtensions.cs
+++ Autobus.Extensions.Hosting/HostBuilderExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Autobus.Implementations;
 using System;
 
 namespace Autobus
@@ -10,32 +9,23 @@ namespace Autobus
         public static IHostBuilder UseAutobus(this IHostBuilder hostBuilder, Action<IAutobusBuilder> onBuild) => 
             hostBuilder.ConfigureServices(services =>
             {
-                var builder = new AutobusBuilder();
-                onBuild(builder);
-                var autobus = builder.Build();
-                services.AddSingleton(autobus);
-                InjectServiceContracts(services, autobus);
+                services.AddSingleton(_ =>
+                {
+                    var builder = new AutobusBuilder();
+                    onBuild(builder);
+                    return builder.Build();
+                });
             });
 
         public static IHostBuilder UseAutobus(this IHostBuilder hostBuilder, Action<HostBuilderContext, IAutobusBuilder> onBuild) => 
             hostBuilder.ConfigureServices((hostBuilderContext, services) =>
             {
-                var builder = new AutobusBuilder();
-                onBuild(hostBuilderContext, builder);
-                var autobus = builder.Build();
-                services.AddSingleton(autobus);
-                InjectServiceContracts(services, autobus);
+                services.AddSingleton(_ =>
+                {
+                    var builder = new AutobusBuilder();
+                    onBuild(hostBuilderContext, builder);
+                    return builder.Build();
+                });
             });
-
-        private static void InjectServiceContracts(IServiceCollection services, IAutobus autobus)
-        {
-            var serviceContracts = autobus.GetServiceContracts();
-            foreach (var serviceContract in serviceContracts)
-            {
-                if (serviceContract is AnonymousServiceContract)
-                    continue;
-                services.AddSingleton(serviceContract.GetType(), serviceContract);
-            }
-        }
     }
 }
