163157207492f7118ee718f8a30b3bfe6b0fbf34
Pythonology <pythonology@gmail.com>
2021-07-15 18:24:00 -0400

    Bring BinaryRecords serializer up-to-date

--- Autobus.Serializers.BinaryRecords/AutobusBuilderExtensions.cs
+++ Autobus.Serializers.BinaryRecords/AutobusBuilderExtensions.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using Autobus.Serialization.BinaryRecords;
-using BinaryRecords;
+﻿using Autobus.Serialization.BinaryRecords;
 
 namespace Autobus
 {
     public static class AutobusBuilderExtensions
     {
-        public static IAutobusBuilder UseBinaryRecordsSerialization(this IAutobusBuilder builder, Action<BinarySerializerBuilder> onSerializerBuild)
-        {
-            var serializerBuilder = new BinarySerializerBuilder();
-            onSerializerBuild(serializerBuilder);
-            var serializer = serializerBuilder.Build();
-            return builder.UseSerializer(new BinaryRecordsSerializationProvider(serializer));
-        }
-
         public static IAutobusBuilder UseBinaryRecordsSerialization(this IAutobusBuilder builder) => builder.UseSerializer<BinaryRecordsSerializationProvider>();
     }
 }
